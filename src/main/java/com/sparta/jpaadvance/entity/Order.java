b12380b3fb635ac312849b8642895eb01e7dfca0
SpringStudy <dnjsdyd78@gmail.com>
2024-08-29 20:11:54 +0900

    중간테이블 오더테스트

---
+++ src/main/java/com/sparta/jpaadvance/entity/Order.java
@@ -0,0 +1,26 @@
+package com.sparta.jpaadvance.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@NoArgsConstructor
+@Entity
+@Getter
+@Setter
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "food_id")
+    private Food food;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
